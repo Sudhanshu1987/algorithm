905a214e925f0c048878936bc55f8e507710ec72
Sudhanshu1987 <sudhanshu.barthwal@gmail.com>
2017-11-05 19:39:44 +0530

    Adding basic tree algorithm

---
+++ Algorithms/BinaryTree.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Node
+    {
+        public Node(int val, Node left, Node right)
+        {
+            value = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int value { get; set; }
+        public Node left { get; set; }
+        public Node right { get; set; }
+    }
+
+    class BinaryTree
+    {
+        Node root = null;
+
+        public Node Root
+        {
+            get
+            {
+                return root;
+            }
+        }
+
+        public void createTree(int[] val)
+        {
+            foreach (int v in val)
+            {
+                root = createNode(root, v);
+            }
+        }
+
+        private Node createNode(Node root, int v)
+        {
+            if(root == null)
+            {
+                Node node = new Node(v,null,null);
+                return node;
+            }
+
+            if(root.value > v)
+            {
+                root.left =  createNode(root.left, v);
+            }
+            else
+            {
+                root.right = createNode(root.right, v);
+            }
+
+            return root;
+        }
+
+        public void postOrder(Node node)
+        {
+            Node rootNode = node;
+
+            Stack<Node> stack = new Stack<Node>();
+
+            while (true)
+            {
+                while (rootNode != null)
+                {
+                    stack.Push(rootNode);
+                    rootNode = rootNode.left;                    
+                }
+
+                if(stack.Count == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    var nd = stack.Pop();
+                    if (stack.Count != 0 && nd == stack.Peek())
+                    {
+                        Console.Write(nd.value + " ");
+                        stack.Pop();
+                    }
+                    else
+                    {                        
+                        if (nd.right != null)
+                        {
+                            rootNode = nd.right;
+                            stack.Push(nd);
+                            stack.Push(nd);
+                        }
+                        else
+                        {
+                            Console.Write(nd.value + " ");
+                        }
+                    }
+                }
+            }
+        }
+
+        public void preOrder(Node node)
+        {
+            if (node == null)
+                return;
+            Console.Write(node.value + "\n");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+
+        public void inOrder(Node node)
+        {
+            if (node == null)
+                return;
+            inOrder(node.left);
+            Console.Write(node.value + "\n");
+            inOrder(node.right);
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+            int[] arr = { 30, 15, 24, 28, 12, 9, 8, 2, 14, 11, 21, 45};
+            binaryTree.createTree(arr);
+            binaryTree.preOrder(binaryTree.Root);
+            binaryTree.inOrder(binaryTree.Root);
+            binaryTree.postOrder(binaryTree.Root);
+        }
+    }
+}
